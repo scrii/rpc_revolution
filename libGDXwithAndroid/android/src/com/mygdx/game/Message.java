64698c33505fa7be452c958ee531cc32601195e5
TomTork <dima26483950@gmail.com>
2021-04-02 18:13:58 +0500

    да

--- libGDXwithAndroid/android/src/com/mygdx/game/Message.java
+++ libGDXwithAndroid/android/src/com/mygdx/game/Message.java
@@ -5,18 +5,36 @@ import java.util.Date;
 public class Message {
 
     private String textMessage;
-    private String autor;
+    private String author;
     private long timeMessage;
-
-    public Message(String textMessage, String autor) {
+    private float coordinats_x;
+    private float coordinats_y;
+    public Message(String textMessage, String author, float coordinats_x, float coordinats_y) {
         this.textMessage = textMessage;
-        this.autor = autor;
+        this.author = author;
+        this.coordinats_x = coordinats_x;
+        this.coordinats_y = coordinats_y;
         timeMessage = new Date().getTime();
     }
 
     public Message() {
     }
 
+    public void setCoordinats_x(float coordinats_x) {
+        this.coordinats_x = coordinats_x;
+    }
+
+    public float getCoordinats_x() {
+        return coordinats_x;
+    }
+
+    public void setCoordinats_y(float coordinats_y) {
+        this.coordinats_y = coordinats_y;
+    }
+
+    public float getCoordinats_y() {
+        return coordinats_y;
+    }
 
     public String getTextMessage() {
         return textMessage;
@@ -29,11 +47,11 @@ public class Message {
 
 
     public String getAutor() {
-        return autor;
+        return author;
     }
 
     public void setAutor(String autor) {
-        this.autor = autor;
+        this.author = autor;
     }
 
     public long getTimeMessage() {
