a9d888fb82b193ecf935fc3ae78d452314136c19
TomTork <dima26483950@gmail.com>
2021-04-05 19:25:19 +0500

    1

--- libGDXwithAndroid/core/src/Online/FirestoreHelper.java
+++ libGDXwithAndroid/core/src/Online/FirestoreHelper.java
@@ -1,13 +1,8 @@
 package Online;
 
-<<<<<<< HEAD
 import com.badlogic.gdx.ai.GdxFileSystem;
 
 import de.tomgrill.gdxfirebase.core.GDXFirebase;
 
 public class FirestoreHelper {
-
-=======
-public class FirestoreHelper {
->>>>>>> origin/main
 }
