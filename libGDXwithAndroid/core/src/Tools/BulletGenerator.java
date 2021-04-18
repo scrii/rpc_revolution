949d6074b926eb3a8cc68992d9dc65716b8dfda3
scrii <79646124+scrii@users.noreply.github.com>
2021-04-18 07:13:44 +0500

    пули с неправильным расположением

--- libGDXwithAndroid/core/src/Tools/BulletGenerator.java
+++ libGDXwithAndroid/core/src/Tools/BulletGenerator.java
@@ -1,5 +1,6 @@
 package Tools;
 
+import com.badlogic.gdx.Gdx;
 import com.teamname.game.Actor.Bullet;
 import com.teamname.game.Main;
 import com.teamname.game.Screens.GameSc;
@@ -9,7 +10,11 @@ public class BulletGenerator {
 
     public void update(Joystick joy){
         isFire= joy.getDir().getX() != 0 || joy.getDir().getY() != 0;
+        //Gdx.app.log("isFIRE",isFire+"");
 
-        if(isFire) GameSc.bullets.add(new Bullet(Main.actor,GameSc.player.position,5,GameSc.player.R,joy.getDir()));
+        if(isFire) {
+            GameSc.bullets.add(new Bullet(Main.actor, GameSc.player.send_in_ONLINE, 7, GameSc.player.R/3, joy.getDir()));
+            //Gdx.app.log("Bullets_m",GameSc.bullets.toString());
+        }
     }
 }
--- libGDXwithAndroid/core/src/com/teamname/game/Actor/Bullet.java
+++ libGDXwithAndroid/core/src/com/teamname/game/Actor/Bullet.java
@@ -1,11 +1,18 @@
 package com.teamname.game.Actor;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.teamname.game.Main;
+import com.teamname.game.Screens.GameSc;
 
 import Tools.Point2D;
+import pl.mk5.gdx.fireapp.GdxFIRDatabase;
 
 public class Bullet extends Actor {
+
+    public boolean isOut;
+    int count;
 
     public Bullet(Texture img, Point2D position, float Speed, float R, Point2D direction) {
         super(img, position, Speed, R);
@@ -19,8 +26,22 @@ public class Bullet extends Actor {
 
     @Override
     public void update() {
-        position.add(direction.getX()*Speed,position.getY()*Speed);
+        position.add(direction.getX()*Speed,direction.getY()*Speed);
         bounds.pos.setPoint(position);
+        //Gdx.app.log("bullet_position", position.toString());
+        isOut = (position.getX()+R<0 || position.getY()-R> Main.BACKGROUND_HEIGHT
+                || position.getX()-R>Main.BACKGROUND_WIDTH || position.getY()+R<0);
 
     }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void removeBullet(int count){
+        //GdxFIRDatabase.instance().inReference("Elbrium_"+count).removeValue();
+        GameSc.bullets.removeIndex(count);
+        //Gdx.app.log("ORE","ORE REMOVED");
+    }
+
 }
