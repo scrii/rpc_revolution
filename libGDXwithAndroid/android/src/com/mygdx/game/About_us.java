6e61c05147b6023b21d807f551f4f15f82820e85
TomTork <dima26483950@gmail.com>
2021-05-13 22:41:37 +0500

    аааааааааааааааааааа

--- libGDXwithAndroid/android/src/com/mygdx/game/About_us.java
+++ libGDXwithAndroid/android/src/com/mygdx/game/About_us.java
@@ -9,8 +9,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class About_us extends AppCompatActivity {
-TextView pr,pr_text,com,j,j_text,l,l_text,an,an_text,a,a_text,e,e_text,bags_tv,vk,vk_text;
-String text,prehistory,prehistory_txt,commands,join,join_txt,leave,leave_txt,an_nickname,an_nickname_txt,all,all_txt,example,example_text,bags,copy_txt,vkon,vkon_txt;
+TextView pr,pr_text,com,j,j_text,l,l_text,an,an_text,a,a_text,e,e_text,bags_tv,vk,vk_text,try_tv,try_text;
+String text,prehistory,prehistory_txt,commands,join,join_txt,leave,leave_txt,an_nickname,an_nickname_txt,all,all_txt,example,example_text,bags,copy_txt,vkon,vkon_txt,try_string_tv,try_string;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +31,10 @@ String text,prehistory,prehistory_txt,commands,join,join_txt,leave,leave_txt,an_
         e = findViewById(R.id.example);
         e_text = findViewById(R.id.example_text);
         bags_tv = findViewById(R.id.bags);
+        try_tv = findViewById(R.id.Try);
+        try_text = findViewById(R.id.Try_text);
+        try_string_tv = "#try ";
+        try_string = "- Возвращает случайное значение: [Успешно] или [Неуспешно].";
         vkon = "Музыка - ";
         vkon_txt = "https://vk.com/ssssadsubmarine";
         prehistory = "Предыстория";
@@ -65,7 +69,10 @@ String text,prehistory,prehistory_txt,commands,join,join_txt,leave,leave_txt,an_
         a_text.setText(all_txt+"\n");
         e.setText(example);
         e.setTextColor(getResources().getColor(R.color.comment));
-        e_text.setText(example_text+"\n\n");
+        e_text.setText(example_text+"\n");
+        try_tv.setText(try_string_tv);
+        try_tv.setTextColor(getResources().getColor(R.color.Try));
+        try_text.setText(try_string+"\n\n");
         bags_tv.setText(bags + copy_txt+"\n");
         vk.setText(vkon);
         vk_text.setText(vkon_txt+"\n");
